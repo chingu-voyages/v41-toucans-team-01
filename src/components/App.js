b70bfa3dc67fb976a0d2cfc784d9557e1d0f372f
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-10-10 18:26:03 +0100

    Add: Create starter files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Main from "./Main";
+
+const App = () => {
+  return (
+    <>
+      <Main />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <main>
+      <h1>Weathrr App</h1>
+    </main>
+  );
+};
+
+export default Main;
