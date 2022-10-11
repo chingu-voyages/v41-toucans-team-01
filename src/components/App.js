c0b823cf6eb2fcaff258a5024ae0f5c9d92c3c6d
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-10-11 20:43:20 +0100

    Import weather api and create api call instance

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 import Main from "./Main";
+import openweathermap from "../apis/openweathermap";
 
 const App = () => {
+  //function to load current weather
+  const loadWeather = async (location) => {
+    const response = await openweathermap.get("/weather", {
+      params: {
+        q: location,
+      },
+    });
+    console.log(response.data);
+  };
+
+  //function to load weather forecast
+  const loadForecast = async (location) => {
+    const response = await openweathermap.get("/forecast", {
+      params: {
+        q: location,
+      },
+    });
+    console.log(response.data);
+  };
   return (
     <>
       <Main />
