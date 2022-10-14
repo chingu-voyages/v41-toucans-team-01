fe2f9db99fd7405d2a677da842ccf44d793d0b0c
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-10-14 12:27:36 +0100

    refactored code and displayed api data as a string in main

--- src/apis/openweathermap.js
+++ src/apis/openweathermap.js
@@ -1,11 +1,38 @@
 import axios from "axios";
 
-const OPENWEATHER_API_KEY = "";
+const OPENWEATHER_API_KEY = "5c707c30b19b0381825e0aaea1f88a5e";
 
-export default axios.create({
+const openweathermap = axios.create({
   baseURL: "https://api.openweathermap.org/data/2.5",
   params: {
     units: "metric",
     APPID: OPENWEATHER_API_KEY,
   },
 });
+
+//function to load current weather
+const loadWeather = async (location) => {
+  const response = await openweathermap.get("/weather", {
+    params: {
+      q: location,
+    },
+  });
+  return response.data;
+};
+
+//function to load weather forecast
+const loadForecast = async (location) => {
+  const response = await openweathermap.get("/forecast", {
+    params: {
+      q: location,
+    },
+  });
+  return response.data;
+};
+
+const weatherApi = {
+  loadWeather,
+  loadForecast,
+};
+
+export default weatherApi;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,28 @@
+import { useState, useEffect } from "react";
 import PrimaryNav from "./navigations/PrimaryNav";
 import SecondaryNav from "./navigations/SecondaryNav";
+import weatherApi from "../apis/openweathermap";
 
 const Main = () => {
+  // Variable to hold/set data in state
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    // Function so that weatherApi method can be called async because
+    // useEffect callback can not be async
+    const getData = async () => {
+      const data = await weatherApi.loadWeather("London, Uk");
+      console.log(data);
+      setWeatherData(data);
+    };
+    getData();
+  }, []);
+
   return (
     <main>
       <PrimaryNav />
       <SecondaryNav />
+      {JSON.stringify(weatherData)}
     </main>
   );
 };
