1888541026b673b7c12698a14fce86cfe0d5851b
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-10-22 20:34:39 +0100

    create weather search bar

---
+++ src/components/search/WeatherSearchBar.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./WeatherSearchBar.css";
+
+const WeatherSearchBar = ({ fetchWeatherData }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchWeatherData(searchTerm);
+  };
+
+  return (
+    <form className="weather-search" onSubmit={handleSubmit}>
+      <input
+        placeholder="Enter a city"
+        className="weather-search-input"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+    </form>
+  );
+};
+
+export default WeatherSearchBar;
