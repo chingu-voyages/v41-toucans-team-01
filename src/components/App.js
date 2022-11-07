e2616772dfcf4e0dea8f81adf9c0753221d91ec5
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-11-07 09:12:33 +0100

    set up routes and protected route

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
 import "./App.css";
-import Main from "./Main";
+import Layout from "./Layout";
+import WeatherPage from "./pages/WeatherPage";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
+import ProfilePage from "./pages/ProfilePage";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+
+const Protected = ({ isLoggedIn, children }) => {
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace={true} />;
+  }
+  return children;
+};
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
   return (
-    <>
-      <Main />
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Protected isLoggedIn={isLoggedIn}>
+            <Layout />
+          </Protected>
+        }
+      >
+        <Route index element={<HomePage />} />
+        <Route path="weather" element={<WeatherPage />} />
+        <Route path="chat" element={<ChatPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+      </Route>
+      <Route path="login" element={<Login />} />
+      <Route path="sign-up" element={<SignUp />} />
+    </Routes>
   );
 };
 
