aea91c82e8b22b6623970fb14f1235cd22bffa56
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-01-15 21:36:24 +0100

    Cleanup: Remove unused routes and file imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,17 @@
 import "./App.css";
-import Layout from "./Layout";
-import WeatherPage from "./weather/WeatherPage";
+import PrimaryNav from "./navigations/PrimaryNav";
+import SecondaryNav from "./navigations/SecondaryNav";
 import HomePage from "./home/HomePage";
-import ChatPage from "./chat/ChatPage";
-import ProfilePage from "./profile/ProfilePage";
-import Login from "./auth/Login";
-import SignUp from "./auth/SignUp";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useState } from "react";
-
-const Protected = ({ isLoggedIn, children }) => {
-  if (!isLoggedIn) {
-    return <Navigate to="/login" replace={true} />;
-  }
-  return children;
-};
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Protected isLoggedIn={isLoggedIn}>
-            <Layout />
-          </Protected>
-        }
-      >
-        <Route index element={<Navigate to="/weather" />} />
-        <Route path="weather" element={<WeatherPage />} />
-        <Route path="chat" element={<ChatPage />} />
-        <Route path="profile" element={<ProfilePage />} />
-      </Route>
-      <Route path="login" element={<Login />} />
-      <Route path="sign-up" element={<SignUp />} />
-    </Routes>
+    <>
+      <PrimaryNav />
+      <main className="content-container">
+        <HomePage />
+      </main>
+      <SecondaryNav />
+    </>
   );
 };
 
